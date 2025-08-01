9344b1537fb8c8bd654aac01fd48a6da16a88961
laxmikurani090 <laxmikurani090@gmail.com>
2025-08-01 15:52:49 +0530

    updated code

--- router/jobapplicationroutes.js
+++ router/jobapplicationroutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 
 const router=express.Router();
-import {applyjob, getapplicationsoflogineduser} from '../controller/jobapplicationcontroller.js';
+import {applyjob, getapplicationsoflogineduser, getapplicationofparticularjob, updateapplication, deleteapplication} from '../controller/jobapplicationcontroller.js';
 
 
 router.post('/apply/:jobid/:userid',applyjob);
 router.get('/apply/:userid',getapplicationsoflogineduser);
-
+router.get('/applicationsbyjobid/:jobid',getapplicationofparticularjob);
+router.put('/applications/:id',updateapplication);
+router.delete('/applications/:id',deleteapplication);
 
 export default router
